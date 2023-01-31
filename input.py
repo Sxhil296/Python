dfe6e79c6bed1269ab4769d4c35e814edd9bfb6e
Sahil <leviack629@gmail.com>
2023-01-31 19:20:28 +0530

    input

--- input.py
+++ input.py
@@ -1,2 +1,3 @@
-name = input('What is your name?  ')
-print('Hello, ' + name)
+name = input('What is your name? ')
+color = input("What is your favorite color? ")
+print(name +' '+  "likes" +' Sahil'+ color)
