545f5f29e9719fc0bd8a0d016db7c72df0ad2d8e
Sahil <leviack629@gmail.com>
2023-02-01 05:26:50 +0530

    math functions

---
+++ mathFun.py
@@ -0,0 +1,19 @@
+# x = 2.9
+# print(round(x))  #3
+
+# x = -2.9
+# print(abs(x))  #2.9
+
+# import math
+# print(math.ceil(3.9))   #4
+
+# import math
+# print(math.floor(3.9))   #3
+
+# import math
+# print(math.factorial(3))  #6
+
+
+
+
+
