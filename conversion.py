b6d3274846e88742f9cad915ab2f5c1447ed7e2f
Sahil <leviack629@gmail.com>
2023-01-31 21:20:33 +0530

    string characteristics

--- conversion.py
+++ conversion.py
@@ -4,6 +4,7 @@
 # print(type(age))
 # print(age)
 
-weight_lbs = input('Weight (lbs): ')
-weight_kg = int(weight_lbs) * 0.45
-print(weight_kg)
+# weight_lbs = input('Weight (lbs): ')
+# weight_kg = int(weight_lbs) * 0.45
+# print(weight_kg)
+
---
+++ strChar.py
@@ -0,0 +1,12 @@
+# course = "Python's"
+# print(course)
+
+# course = 'Python course for "beginners"'
+# print(course)
+
+course = '''
+Hii Sahil,
+How are you doing?
+Hope you are doing fine!
+'''
+print(course)
