e7b6bfca8567571b2764e85978cdce8b107b1b38
Sahil <leviack629@gmail.com>
2023-01-31 21:34:25 +0530

    formatted str

---
+++ formattedStr.py
@@ -0,0 +1,8 @@
+first_name = 'Paterick'
+last_name = 'Bateman'
+message = first_name + ' ' + last_name + ' is the American Psycho.'
+msg = f'{first_name} {last_name} is the American Psycho.'
+print(message)
+print(msg)
+
+#both will give the same output.
