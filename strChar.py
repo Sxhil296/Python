8a7d0a532c7dc4f7c86d65b616951332d7ebc1ec
Sahil <leviack629@gmail.com>
2023-01-31 21:22:37 +0530

    string characteristics

--- strChar.py
+++ strChar.py
@@ -4,9 +4,13 @@
 # course = 'Python course for "beginners"'
 # print(course)
 
-course = '''
-Hii Sahil,
-How are you doing?
-Hope you are doing fine!
-'''
-print(course)
+# course = '''
+# Hii Sahil,
+# How are you doing?
+# Hope you are doing fine!
+# '''
+# print(course)
+
+name = 'David'
+       #01234
+print(name[0])
