9175c875db0a31565536df07c9c46a62ea09ea5f
Sahil <leviack629@gmail.com>
2023-02-01 05:20:53 +0530

    numbers

--- num.py
+++ num.py
@@ -11,3 +11,16 @@ print(x)
 y = 10
 y *= 4
 print(y)
+
+a = 10 + 3 * 4 ** 2
+print(a)
+
+b = 10 * 4 / 2
+print(b)
+
+
+c = 10 + 6 - 3
+print(c)
+
+#operator precendence
+#parantehsis>exponention> division > multiplication > subtraction >addition
