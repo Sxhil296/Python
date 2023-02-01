46aedd55fdf7df0377b434311220483f0b42b85e
Sahil <leviack629@gmail.com>
2023-02-01 09:46:57 +0530

    logical operator

--- logical.py
+++ logical.py
@@ -12,3 +12,11 @@
 #     print("Eligible for loan.")
 # else:
 #     print("Not eligible for loan")
+
+
+# has_criminal_record = False
+# has_good_credit = True
+# if has_good_credit and not has_criminal_record:
+#     print("Eligible for loan.")
+# else:
+#     print("Not eligible for loan")
