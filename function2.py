e19bcbc0a7a56dea6d91d7c0cf9ecb4ccc3dfce1
Sahil <leviack629@gmail.com>
2023-02-01 21:16:18 +0530

    function

--- function2.py
+++ function2.py
@@ -1,9 +1,9 @@
-def greet_user(name):   #funtion definition
-    print(f"Hi {name}!")
+def greet_user(first_name, last_name):   #funtion definition
+    print(f"Hi {first_name} {last_name}!")
     print("Welcome aboard")
 
 
 print("Start")
-greet_user("Paterick")  #function call
-greet_user("Bateman")
+greet_user("Paterick", "Bateman")  #function call
+greet_user("Ryan", "Ghosling")
 print("Finish")
