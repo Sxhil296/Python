45ff1c93fccce73e394ed18168c00a28915e10af
Sahil <leviack629@gmail.com>
2023-02-01 09:44:06 +0530

    logical operator

---
+++ logical.py
@@ -0,0 +1,14 @@
+# has_good_credit = True
+# has_high_income = True
+# if has_high_income and has_good_credit:
+#     print("Eligible for loan.")
+# else:
+#     print("Not eligible for loan")
+
+
+# has_good_credit = False
+# has_high_income = False
+# if has_high_income or has_good_credit:
+#     print("Eligible for loan.")
+# else:
+#     print("Not eligible for loan")
