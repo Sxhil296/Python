93f221a38fe40f700373f25218786ba0f65cc729
Sahil <leviack629@gmail.com>
2023-02-01 15:33:09 +0530

    for loop

---
+++ forLoop.py
@@ -0,0 +1,15 @@
+# for item in 'Python':
+#     print(item)
+
+
+# for item in ['David', 'John', 'Paterick', 'Ryan']:
+#     print(item)
+
+# for item in range(10):
+#     print(item)
+
+# for i in range(5, 10):
+#     print(i)
+
+for i in range(5, 15, 2):
+    print(i)
