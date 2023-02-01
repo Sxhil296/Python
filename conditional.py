f94420fba943908caca4ca0953bb87d870d12230
Sahil <leviack629@gmail.com>
2023-02-01 05:38:23 +0530

    if conditional

--- conditional.py
+++ conditional.py
@@ -19,14 +19,23 @@
 #     print("Wear warm clothes.")
 # print("Enjoy your day!")
 
-is_hot = False
-is_cold = False
-if is_hot:
-    print("It's a hot day.")
-    print("Stay Hydrated!")
-elif is_cold:
-    print("It's a cool day.")
-    print("Wear warm clothes.")
+# is_hot = False
+# is_cold = False
+# if is_hot:
+#     print("It's a hot day.")
+#     print("Stay Hydrated!")
+# elif is_cold:
+#     print("It's a cool day.")
+#     print("Wear warm clothes.")
+# else:
+#     print("It's a lovely day.")
+# print("Enjoy your day!")
+
+
+price = 1000000
+has_good_credit = True
+if has_good_credit:
+    down_payment = 0.1 * price
 else:
-    print("It's a lovely day.")
-print("Enjoy your day!")
+    down_payment = 0.2 * price
+print(f"Down Payment: ${down_payment}")
