e2907ffc840637f450f2d4cdec5f553a53f6921b
Sahil <leviack629@gmail.com>
2023-02-01 17:40:13 +0530

    list methods

---
+++ listMethods.py
@@ -0,0 +1,29 @@
+# numbers = [5, 2, 1, 6, 4]
+# numbers.append(13)
+# print(numbers)
+
+# numbers = [5, 2, 1, 6, 4]
+# numbers.insert(0, 10)
+# print(numbers)
+
+
+# numbers = [5, 2, 1, 6, 4]
+# numbers.remove(5)
+# print(numbers)
+
+
+# numbers = [5, 2, 1, 6, 4]
+# numbers.clear()
+# print(numbers)
+
+# numbers = [5, 2, 1, 6, 4]
+# numbers.pop()
+# print(numbers)
+
+# numbers = [5, 2, 1, 6, 4]
+# print(numbers.index(1))
+
+# numbers = [5, 2, 1, 6, 4]
+# print(50 in numbers)
+
+
