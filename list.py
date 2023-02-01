26537c654ee8569a061fada9f3f8890c82d74814
Sahil <leviack629@gmail.com>
2023-02-01 16:18:52 +0530

    lists

--- list.py
+++ list.py
@@ -4,7 +4,20 @@
 # names = ['Bob', 'James', 'David', 'Bateman']
 # print(names[-1])
 
-names = ['Bob', 'James', 'David', 'Bateman', 'Paterick']
-print(names[1: 4])
+# names = ['Bob', 'James', 'David', 'Bateman', 'Paterick']
+# print(names[1: 4])
 
+# names = ['Bob', 'James', 'David', 'Bateman', 'Paterick']
+# names[0] = 'Batman'
+# print(names)
+
+
+#find the largest number in the list
+
+numbers = [3, 4, 7, 8, 22, 10]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
 
