732882efeb4088595d5f5e6d62057422c4a6fb91
Sahil <leviack629@gmail.com>
2023-02-01 09:54:16 +0530

    comparison operator

--- comparison.py
+++ comparison.py
@@ -1,6 +1,13 @@
-temprature = 35
+# temprature = 35
+# if temprature > 30:
+#     print("it's a hot day")
+# else:
+#     print("It's not a hot day")
 
-if temprature > 30:
-    print("it's a hot day")
+name = "Paterick Bateman"
+if len(name) < 3:
+    print("Name must be at least 3 characters.")
+elif len(name) > 50:
+    print("Name must be at max of 50 characters.")
 else:
-    print("It's not a hot day")
+    print("Name looks good.")
