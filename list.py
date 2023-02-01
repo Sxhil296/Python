3ba9123e4d99617697226b789ab4a991eb4a4ca1
Sahil <leviack629@gmail.com>
2023-02-01 16:14:39 +0530

    lists

---
+++ list.py
@@ -0,0 +1,10 @@
+# names = ['Bob', 'James', 'David', 'Bateman']
+# print(names)
+
+# names = ['Bob', 'James', 'David', 'Bateman']
+# print(names[-1])
+
+names = ['Bob', 'James', 'David', 'Bateman', 'Paterick']
+print(names[1: 4])
+
+
