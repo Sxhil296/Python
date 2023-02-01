9098d1e54fbd4082f2d84986da811f140bfbe760
Sahil <leviack629@gmail.com>
2023-02-01 05:34:22 +0530

    if conditional

--- conditional.py
+++ conditional.py
@@ -10,11 +10,23 @@
 #     print("It's a hot day.")
 # print("Enjoy your day!")
 
+# is_hot = False
+# if is_hot:
+#     print("It's a hot day.")
+#     print("Stay Hydrated!")
+# else:
+#     print("It's a cool day.")
+#     print("Wear warm clothes.")
+# print("Enjoy your day!")
+
 is_hot = False
+is_cold = False
 if is_hot:
     print("It's a hot day.")
     print("Stay Hydrated!")
-else:
+elif is_cold:
     print("It's a cool day.")
     print("Wear warm clothes.")
+else:
+    print("It's a lovely day.")
 print("Enjoy your day!")
