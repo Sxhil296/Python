a533bb1e896c2e3f4ee57576549b93787182a866
Sahil <leviack629@gmail.com>
2023-02-01 21:39:14 +0530

    exception error

--- error.py
+++ error.py
@@ -1,5 +1,9 @@
 try:
     age = int(input("Age: "))
+    income = 20000
+    risk = income / age
     print(age)
+except ZeroDivisionError:
+    print("Age can't be zero.")
 except ValueError:
     print("Invalid Value!")
