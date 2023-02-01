739b22729bf0d74405d81ea3aa4e3ab621573262
Sahil <leviack629@gmail.com>
2023-02-01 18:45:29 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+student = {
+    "name": 'John',
+    "age": 21,
+    "is_graduated": True
+}
+
+# print(student["name"])
+# print(student.get("age"))
+# print(student.get("dob", "jan 1 1999"))
+
+student["name"] = "paterick"
+print(student["name"])
+student["dob"] = "jan 1 1999"
+print(student["dob"])
