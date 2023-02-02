8b740f2700f86a802ffcadcb9947ce1c07b3cdc8
Sahil <leviack629@gmail.com>
2023-02-02 23:30:25 +0530

    random number

---
+++ buildInModules.py
@@ -0,0 +1,10 @@
+import random
+
+# for i in range(3):
+#     print(random.random())
+#     print(random.randint(10, 20))
+
+# members = ['John', "Marry", 'Bob', 'Smash']
+# leader = random.choice(members)
+# print(leader)
+
